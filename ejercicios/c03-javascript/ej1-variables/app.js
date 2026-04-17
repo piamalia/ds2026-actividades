51d66e1470bd321ea85fc562a73db0de8ed23f35
Andrea Sofia Vena <andreasofiavena@gmail.com>
2026-04-16 23:16:09 -0300

    Ejercicio 1: variables y operaciones

---
+++ ejercicios/c03-javascript/ej1-variables/app.js
@@ -0,0 +1,13 @@
+const nombre = "Sofia";
+const edad = 25;
+const materia = "Desarrollo de software";
+
+console.log(`Me llamo ${nombre}, tengo ${edad} años y curso ${materia}`);
+
+let contador = 0;
+
+contador = contador + 1;
+contador = contador + 1;
+contador = contador + 1;
+
+console.log(contador);
